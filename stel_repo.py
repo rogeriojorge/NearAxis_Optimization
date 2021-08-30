5d4bdb154b876b5ed9fe6c11b2b2dfadc7b580b6
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2021-08-30 23:58:58 +0100

    update

--- stel_repo.py
+++ stel_repo.py
@@ -262,7 +262,7 @@ def get_stel(ind,nphi=251):
         p2     =  -1000000.0
         iota   =  0.41059495901415516
         stel   =  make_optimizable(Qsc(rc=rc, zs=zs, etabar=etabar, nfp=nfp, nphi=nphi, B2c=B2c, order='r3', p2=p2))
-        r_edge =  1/6
+        r_edge =  1/14
         coilSeparation = 0.1
         targetValue = 0.08
         nCoilsPerNFP = 6
