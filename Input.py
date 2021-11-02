fade64e2c3ffa7b1c0c5959f8c455e01125aa5d8
Rogerio Jorge <rogeriodejesusjorge@gmail.com>
2021-11-02 19:05:31 -0400

    results qi 2nd order nfp1, 2

--- Input.py
+++ Input.py
@@ -1,17 +1,17 @@
 ind=30
-nphi=71
+nphi=331
 
 iota_target = 0.42
 nIterations = 50
 abs_step_array = [5e-2,1e-2,1e-3,1e-4,1e-6]
 rel_step_array = [1e-1,1e-2]
 max_fourier_coefficients = 6
-r_edge = 1/40
+r_edge = 1/30
 
-Optimize = True
+# Optimize = True
 Plot = True
-# VMEC = True
-# BOOZ_XFORM = True
-# NEO = True
-# SPEC = True
+VMEC = True
+BOOZ_XFORM = True
+NEO = True
+SPEC = True
 # REGCOIL = True
