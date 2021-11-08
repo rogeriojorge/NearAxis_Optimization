ccc774a2536f1c471842d38759ff063f521d4e6b
Rogerio Jorge <rogeriodejesusjorge@gmail.com>
2021-11-08 17:01:37 -0500

    same

--- Input.py
+++ Input.py
@@ -1,4 +1,4 @@
-ind=30
+ind=31
 nphi=301
 
 iota_target = 0.42
@@ -6,7 +6,7 @@ nIterations = 150
 abs_step_array = [1e-1,1e-2,1e-3,1e-4,1e-6]
 rel_step_array = [1e-1,1e-2,1e-3]
 max_fourier_coefficients = 8
-r_edge = 1/15
+r_edge = 1/25
 ftol = 1e-3
 
 # Optimize = True
