d5c20cbdd23f9327ad0c8d06fa2d3d489f5cbc62
Rogerio Jorge <rogeriodejesusjorge@gmail.com>
2021-11-17 04:54:25 -0500

    new NFP1_r2 that does not look too good

--- Input.py
+++ Input.py
@@ -1,12 +1,12 @@
-ind=27
+ind=28
 nphi=101
 
 iota_target = 0.42
 nIterations = 50
 abs_step_array = [1e-1,1e-2,1e-3,1e-4,1e-6]
 rel_step_array = [1e-1,1e-2]
-max_fourier_coefficients = 8
-r_edge = 1/15
+max_fourier_coefficients = 10
+r_edge = 1/20
 ftol = 1e-3
 
 Optimize = True
--- main.py
+++ main.py
@@ -88,8 +88,8 @@ try:
         print('  B_fieldline()')
         stel.B_fieldline(r=r_edge, savefig='pyQSC_out.'+name, show=False)
         print('  plot_boundary()')
-        stel.plot_boundary(r=r_edge, fieldlines=True, savefig='pyQSC_out.'+name+'.boundary', show=False)
-        # stel.plot_boundary(r=r_edge, fieldlines=False, savefig='pyQSC_out.'+name+'.boundary', show=False, ntheta=120, nphi=int(120*stel.nfp), ntheta_fourier=30)
+        # stel.plot_boundary(r=r_edge, fieldlines=True, savefig='pyQSC_out.'+name+'.boundary', show=False)
+        stel.plot_boundary(r=r_edge, fieldlines=False, savefig='pyQSC_out.'+name+'.boundary', show=False, ntheta=120, nphi=int(120*stel.nfp), ntheta_fourier=30)
 except Exception as e:
     print(e)
     Input.Plot = False
