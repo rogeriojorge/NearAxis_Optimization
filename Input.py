206db1296dc3a89a04ee2d5a1907c7a75c4733fe
Rogerio Jorge <rogeriodejesusjorge@gmail.com>
2021-11-23 04:41:20 -0500

    I think I got it

--- Input.py
+++ Input.py
@@ -6,12 +6,12 @@ nIterations = 150
 abs_step_array = [1e-1,1e-2,1e-3,1e-4,1e-6]
 rel_step_array = [1e-1,1e-2]
 max_fourier_coefficients = 4
-r_edge = 1/8
+r_edge = 1/12
 ftol = 1e-3
 
 # Optimize = True
-# Plot = True
-# VMEC = True
+Plot = True
+VMEC = True
 BOOZ_XFORM = True
 NEO = True
 # SPEC = True
