9e54f492ef71fb3541472011105e71b4b006fa0e
Rogerio Jorge <rogeriodejesusjorge@gmail.com>
2021-12-16 12:25:24 -0500

    better NFP=2 configuration

--- Input.py
+++ Input.py
@@ -3,10 +3,10 @@ nphi=131
 
 iota_target = 0.42
 nIterations = 150
-abs_step_array = [1e-1,1e-2,1e-3,1e-4,1e-6]
-rel_step_array = [1e-1,1e-2]
-max_fourier_coefficients = 6
-r_edge = 1/15
+# abs_step_array = [1e-1,1e-2,1e-3,1e-4,1e-6]
+# rel_step_array = [1e-1,1e-2,1e-3]
+max_fourier_coefficients = 10
+r_edge = 1/9
 ftol = 1e-4
 
 Optimize = True
