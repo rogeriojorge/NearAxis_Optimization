a3d31996819c8546008a22b95a0ee47d5b59310b
Rogerio Jorge <rogeriodejesusjorge@gmail.com>
2021-12-16 13:33:16 -0500

    NFP=3 configuration

--- Input.py
+++ Input.py
@@ -5,7 +5,7 @@ iota_target = 0.42
 nIterations = 150
 # abs_step_array = [1e-1,1e-2,1e-3,1e-4,1e-6]
 # rel_step_array = [1e-1,1e-2,1e-3]
-max_fourier_coefficients = 3
+max_fourier_coefficients = 10
 r_edge = 1/14
 ftol = 1e-4
 
